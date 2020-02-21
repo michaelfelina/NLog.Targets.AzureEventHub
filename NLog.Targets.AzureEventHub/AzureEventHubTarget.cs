9cdeeb5fccc0ede0bbbeaa28b5dfa6ce1385a201
Michael Felina <michael.felina@attomdata.com>
2020-02-21 09:57:42 +0800

    Initial Commit

---
+++ NLog.Targets.AzureEventHub/AzureEventHubTarget.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Azure.EventHubs;
+using Newtonsoft.Json;
+using NLog.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog.Targets.AzureEventHub
+{
+    [Target("AzureEventHub")]
+    public partial class AzureEventHubTarget : TargetWithLayout
+    {
+        static EventHubClient _eventHubClient = null;
+
+        public string PartitionKey { get; set; }
+        [RequiredParameter]
+        public string EventHubConnectionString { get; set; }
+
+        [RequiredParameter]
+        public string EventHubPath { get; set; }
+
+        /// <summary>
+        /// Takes the contents of the LogEvent and sends the message to EventHub
+        /// </summary>
+        /// <param name="logEvent"></param>
+        /// 
+        
+        protected override void Write(LogEventInfo logEvent)
+        {
+            SendAsync(PartitionKey, logEvent);
+        }
+
+        private async Task<bool> SendAsync(string partitionKey, LogEventInfo logEvent)
+        {
+            try
+            {             
+                if (_eventHubClient == null)
+                    _eventHubClient = EventHubClient.CreateFromConnectionString(EventHubConnectionString);
+                var eventHubData = CreateEventData(logEvent);
+                await _eventHubClient.SendAsync(eventHubData);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return false;
+        }
+
+        private EventData CreateEventData(LogEventInfo data)
+        {
+            var logInformation = new LogEvent
+            {
+                SequenceID = data.SequenceID,
+                TimeStamp = data.TimeStamp,
+                Level = data.Level.ToString(),
+                LoggerName = data.LoggerName,
+                Message = data.FormattedMessage,
+                Exception = data.Exception?.ToString(),
+                CallerClassName = data.CallerClassName,
+                CallerFilePath = data.CallerFilePath,
+                CallerLineNumber = data.CallerLineNumber,
+                CallerMemberName = data.CallerMemberName,
+                FormatProvider = data.FormatProvider?.ToString(),
+                FormattedMessage = data.FormattedMessage,
+                HasProperties = data.HasProperties,
+                HasStackTrace = data.HasStackTrace,
+                MessageTemplateParameters = data.MessageTemplateParameters?.ToString(),
+                Properties = data.Properties,
+                StackTrace = data.StackTrace?.ToString(),
+                UserStackFrame = data.UserStackFrame,
+                UserStackFrameNumber = data.UserStackFrameNumber,                
+            };
+
+            var dataAsJson = JsonConvert.SerializeObject(logInformation);
+            var eventHubData = new EventData(Encoding.UTF8.GetBytes(dataAsJson));
+            return eventHubData;
+        }
+    } 
+}
---
+++ NLog.Targets.AzureEventHub/LogEvent.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace NLog.Targets.AzureEventHub
+{
+    public class LogEvent
+    {
+        //
+        // Summary:
+        //     Gets the callsite class name
+        public string CallerClassName { get; set; }
+        //
+        // Summary:
+        //     Gets the callsite member function name
+        public string CallerMemberName { get; set; }
+        //
+        // Summary:
+        //     Gets the callsite source file path
+        public string CallerFilePath { get; set; }
+        //
+        // Summary:
+        //     Gets the callsite source file line number
+        public int CallerLineNumber { get; set; }
+        //
+        // Summary:
+        //     Gets or sets the exception information.        
+        public string Exception { get; set; }
+        //
+        // Summary:
+        //     Gets or sets the logger name.
+        public string LoggerName { get; set; }
+
+        //
+        // Summary:
+        //     Gets or sets the format provider that was provided while logging or null when
+        //     no formatProvider was specified.
+        public string FormatProvider { get; set; }
+        //
+        // Summary:
+        //     Gets the entire stack trace.
+        public string StackTrace { get; set; }
+        //
+        // Summary:
+        //     Gets or sets the message formatter for generating NLog.LogEventInfo.FormattedMessage
+        //     Uses string.Format(...) when nothing else has been configured.
+        public string MessageFormatter { get; set; }
+        //
+        // Summary:
+        //     Gets the formatted message.
+        public string FormattedMessage { get; set; }
+        //
+        // Summary:
+        //     Checks if any per-event properties (Without allocation)
+        public bool HasProperties { get; set; }
+        //
+        // Summary:
+        //     Gets the dictionary of per-event context properties.
+        public IDictionary<object, object> Properties { get; set; }
+        //
+        // Summary:
+        //     Gets or sets the log message including any parameter placeholders.
+        public string Message { get; set; }
+        //
+        // Summary:
+        //     Gets the number index of the stack frame that represents the user code (not the
+        //     NLog code).
+        public int UserStackFrameNumber { get; set; }
+        //
+        // Summary:
+        //     Gets or sets the level of the logging event.
+        public string Level { get; set; }
+        //
+        // Summary:
+        //     Gets a value indicating whether stack trace has been set for this event.
+        public bool HasStackTrace { get; set; }
+        //
+        // Summary:
+        //     Gets the stack frame of the method that did the logging.
+        public StackFrame UserStackFrame { get; set; }
+        //
+        // Summary:
+        //     Gets the unique identifier of log event which is automatically generated and
+        //     monotonously increasing.
+        public int SequenceID { get; set; }
+
+        //
+        // Summary:
+        //     Gets the named parameters extracted from parsing NLog.LogEventInfo.Message as
+        //     MessageTemplate
+        public string MessageTemplateParameters { get; set; }
+        //
+        // Summary:
+        //     Gets or sets the timestamp of the logging event.
+        public DateTime TimeStamp { get; set; }
+    }
+}
